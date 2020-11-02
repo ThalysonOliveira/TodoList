d587075a0df2fa7d2ab39646985d8fbf5eb7f2e2
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-02 16:22:43 -0200

    Criado componet TodoList

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,12 @@
+import Reac, { Component } from 'react'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <h1></h1>
+            </div>
+        )
+    }
+}
+

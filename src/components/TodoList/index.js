df8b699f7c488c7b2611eb2287f95a56126e0d41
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-02 16:42:57 -0200

    Criado metodo addItem

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,22 +1,37 @@
 import Reac, { Component } from 'react'
 
 export default class TodoList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             tarefa: '',
             items: []
         }
+        this.addItem = this.addItem.bind(this)
     }
+
+    addItem(e) {
+        let state = this.state
+        if (this._tarefaInput.value !== '') {
+            let newItem = {
+                text: this._tarefaInput.value,
+                key: Date.now()
+            }
+            this.setState({ items: [...state.items, newItem] })
+        }
+        e.preventDefault()
+        this.setState({tarefa: ''})
+    }
+
     render() {
         return (
             <div>
                 <form onSubmit={this.addItem}>
                     <input type="text"
-                            placeholder="Nova Tarefa"
-                            value={this.state.tarefa}
-                            onChange={(e)=>this.setState({tarefa:e.target.value})}
-                            ref={(event)=>this._tarefaInput = event}/>
+                        placeholder="Nova Tarefa"
+                        value={this.state.tarefa}
+                        onChange={(e) => this.setState({ tarefa: e.target.value })}
+                        ref={(event) => this._tarefaInput = event} />
                     <button type="submit">Adicionar</button>
                 </form>
             </div>

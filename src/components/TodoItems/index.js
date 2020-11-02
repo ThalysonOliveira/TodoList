3c806bed0e0ed1c1fef5d7dd194eee4f08fc4f83
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-11-02 16:51:02 -0200

    Criado component TodoItems

---
+++ src/components/TodoItems/index.js
@@ -0,0 +1,16 @@
+import Reac, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { }
+    }
+
+    render() {
+        return (
+           <div>
+
+           </div>
+        )
+    }
+}
